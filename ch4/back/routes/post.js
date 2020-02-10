7a95ebeca7174e67459a0f9f9d8d4c700f74d2bd
pius712 <aksfbsgnlfls@gmail.com>
2020-02-10 20:59:09 +0900

    backup

--- ch4/back/routes/post.js
+++ ch4/back/routes/post.js
@@ -7,61 +7,70 @@ const { isLoggedIn } = require('./middleware');
 const router = express.Router();
 
 const upload = multer({
-  storage: multer.diskStorage({
-    destination(req, file, done) {
-      done(null, 'uploads');
-    },
-    filename(req, file, done) {
-      const ext = path.extname(file.originalname); // 확장자를 뽑아오는 것.
-      const basename = path.basename(file.originalname, ext); // pius.png, basename = pius, ext = .png
-      done(null, basename + Date.now() + ext);
-    },
-  }),
-  limit: { fileSize: 20 * 1024 * 1024 }, // 사이즈 제한
+	storage: multer.diskStorage({
+		destination(req, file, done) {
+			done(null, 'uploads');
+		},
+		filename(req, file, done) {
+			const ext = path.extname(file.originalname); // 확장자를 뽑아오는 것.
+			const basename = path.basename(file.originalname, ext); // pius.png, basename = pius, ext = .png
+			done(null, basename + Date.now() + ext);
+		},
+	}),
+	limit: { fileSize: 20 * 1024 * 1024 }, // 사이즈 제한
 });
 router.post('/images', isLoggedIn, upload.array('image'), (req, res) => {
-  console.log(req.files);
-  res.json(req.files.map(v => v.filename));
+	console.log(req.files);
+	res.json(req.files.map(v => v.filename));
 });
 
 router.post('/', isLoggedIn, async (req, res, next) => {
-  try {
-    // req.body.content === '안녕하세요~~ #nodejs #vue' ,
-    //req.body.imagePaths
-    const hashtags = req.body.content.match(/#[^\s#]+/g);
-    const newPost = await db.Post.create({
-      content: req.body.content,
-      UserId: req.user.id,
-    });
-    if (hashtags) {
-      // map 함수의 결과값은 배열인데 배열의 인자들이 다 promise이다.
-      // 왜냐하면 db.Hashtag.findOrCreate의 return이 Promise이기 때문.
-      const result = await Promise.all(
-        hashtags.map(tag =>
-          db.Hashtag.findOrCreate({
-            // 있으면 저장하지말고 없으면 저장.
-            where: {
-              name: tag.slice(1).toLowerCase(),
-            },
-          }),
-        ),
-      );
-      await newPost.addHashtags(result.map(r => r[0]));
-    }
-    // 이거는 sequelize가 만들어주는 함수.. ㅜ.ㅜ;;
-    const fullPost = await db.Post.findOne({
-      where: { id: newPost.id },
-      include: [
-        {
-          model: db.User,
-          attributes: ['id', 'nickname'],
-        },
-      ],
-    });
-    return res.json(fullPost);
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
+	try {
+		// req.body.content === '안녕하세요~~ #nodejs #vue' ,
+		//req.body.imagePaths
+		const hashtags = req.body.content.match(/#[^\s#]+/g);
+		const newPost = await db.Post.create({
+			content: req.body.content,
+			UserId: req.user.id,
+		});
+		if (hashtags) {
+			// map 함수의 결과값은 배열인데 배열의 인자들이 다 promise이다.
+			// 왜냐하면 db.Hashtag.findOrCreate의 return이 Promise이기 때문.
+			const result = await Promise.all(
+				hashtags.map(tag =>
+					db.Hashtag.findOrCreate({
+						// 있으면 저장하지말고 없으면 저장.
+						where: {
+							name: tag.slice(1).toLowerCase(),
+						},
+					}),
+				),
+			);
+			// result.map(r => {
+			// 	console.log('result');
+			// 	console.log('r', r);
+			// 	console.log('r[0]', r[0]);
+			// });
+			// 여기서 r 은 유사배열이다... r[0]에 모델 인스턴스가 들어간다라고 생각하면 된다.
+			// 공식문서 : In this example, findOrCreate returns an array like this:
+			// 그리고 await 붙으면 promise안에 value가 result에 들어간다.
+			await newPost.addHashtags(result.map(r => r[0]));
+			// 이렇게하면 중간에 relationship에 추가 된다.
+		}
+		// 이거는 sequelize가 만들어주는 함수.. ㅜ.ㅜ;;
+		const fullPost = await db.Post.findOne({
+			where: { id: newPost.id },
+			include: [
+				{
+					model: db.User,
+					attributes: ['id', 'nickname'],
+				},
+			],
+		});
+		return res.json(fullPost);
+	} catch (err) {
+		console.error(err);
+		next(err);
+	}
 });
 module.exports = router;
--- ch4/front/components/PostForm.vue
+++ ch4/front/components/PostForm.vue
@@ -1,104 +1,104 @@
 <template>
-  <v-card>
-    <v-container>
-      <v-form ref="form" v-model="valid" @submit.prevent="onSubmitForm">
-        <v-textarea
-          v-model="content"
-          outlined
-          auto-grow
-          clearable
-          label="무슨 일이 있었는지 공유하세요"
-          :hide-details="hideDetails"
-          :success="success"
-          :success-messages="successMessages"
-          :rules="[v => !!v.trim() || '입력하라구~']"
-          @input="onChangeTextareas"
-        ></v-textarea>
-        <v-btn type="submit" absolute right>업로드</v-btn>
-        <input
-          ref="imageInput"
-          type="file"
-          multilple
-          hidden
-          @change="onChangeImages"
-        />
-        <v-btn type="button" @click="onClickImageUpload">이미지 업로드</v-btn>
-        <div v-for="(p, i) in imagePaths" :key="i" style="display:inline-block">
-          <img :src="`http://localhost:8080/${p}`" style="width: 200px" />
-          <div>
-            <button type="button" @click="onRemoveImage(i)">제거</button>
-          </div>
-        </div>
-      </v-form>
-    </v-container>
-  </v-card>
+	<v-card>
+		<v-container>
+			<v-form ref="form" v-model="valid" @submit.prevent="onSubmitForm">
+				<v-textarea
+					v-model="content"
+					outlined
+					auto-grow
+					clearable
+					label="무슨 일이 있었는지 공유하세요"
+					:hide-details="hideDetails"
+					:success="success"
+					:success-messages="successMessages"
+					:rules="[v => !!v.trim() || '입력하라구~']"
+					@input="onChangeTextareas"
+				></v-textarea>
+				<v-btn type="submit" absolute right>업로드</v-btn>
+				<input
+					ref="imageInput"
+					type="file"
+					multilple
+					hidden
+					@change="onChangeImages"
+				/>
+				<v-btn type="button" @click="onClickImageUpload">이미지 업로드</v-btn>
+				<div v-for="(p, i) in imagePaths" :key="i" style="display:inline-block">
+					<img :src="`http://localhost:8080/${p}`" style="width: 200px" />
+					<div>
+						<button type="button" @click="onRemoveImage(i)">제거</button>
+					</div>
+				</div>
+			</v-form>
+		</v-container>
+	</v-card>
 </template>
 
 <script>
 import { mapState } from 'vuex';
 export default {
-  data() {
-    return {
-      valid: false,
-      hideDetails: true,
-      success: false,
-      successMessages: '',
-      content: '',
-    };
-  },
-  computed: {
-    ...mapState('users', ['me']),
-    ...mapState('posts', ['imagePaths']),
-  },
-  methods: {
-    onChangeTextareas(value) {
-      if (value.length) {
-        this.hideDetails = true;
-        this.success = false;
-        this.successMessage = '';
-      }
-      //글자 칠때마다 발생하는 메서드 input
-    },
-    onSubmitForm() {
-      if (this.$refs.form.validate()) {
-        console.log(this.me);
-        this.$store
-          .dispatch('posts/add', {
-            content: this.content,
+	data() {
+		return {
+			valid: false,
+			hideDetails: true,
+			success: false,
+			successMessages: '',
+			content: '',
+		};
+	},
+	computed: {
+		...mapState('users', ['me']),
+		...mapState('posts', ['imagePaths']),
+	},
+	methods: {
+		onChangeTextareas(value) {
+			if (value.length) {
+				this.hideDetails = true;
+				this.success = false;
+				this.successMessage = '';
+			}
+			//글자 칠때마다 발생하는 메서드 input
+		},
+		onSubmitForm() {
+			if (this.$refs.form.validate()) {
+				console.log(this.me);
+				this.$store
+					.dispatch('posts/add', {
+						content: this.content,
 
-            // content: this.content,
-            // User: {
-            //   nickname: this.me.nickname,
-            // },
-            // Comments: [],
-            // images: [],
-            // id: Date.now(),
-            // createdAt: Date.now(),
-          })
-          .then(() => {
-            this.hideDetails = false;
-            this.success = true;
-            this.successMessages = '게시글 등록 성공';
-            this.content = '';
-          });
-      }
-    },
-    onClickImageUpload() {
-      this.$refs.imageInput.click();
-    },
-    onChangeImages(e) {
-      console.log(e.target.files);
-      const imageFormData = new FormData();
-      [].forEach.call(e.target.files, f => {
-        // e.target.files가 유사배열이라서, forEach를 못쓴다. forEach는 인자가 콜백함수.
-        imageFormData.append('image', f);
-      });
-      this.$store.dispatch('posts/uploadImages', imageFormData);
-    },
-    onRemoveImage(idx) {
-      this.$store.commit('posts/removeImagePath', idx);
-    },
-  },
+						// content: this.content,
+						// User: {
+						//   nickname: this.me.nickname,
+						// },
+						// Comments: [],
+						// images: [],
+						// id: Date.now(),
+						// createdAt: Date.now(),
+					})
+					.then(() => {
+						this.hideDetails = false;
+						this.success = true;
+						this.successMessages = '게시글 등록 성공';
+						this.content = '';
+					});
+			}
+		},
+		onClickImageUpload() {
+			this.$refs.imageInput.click();
+		},
+		onChangeImages(e) {
+			console.log(e.target.files);
+			const imageFormData = new FormData();
+			[].forEach.call(e.target.files, f => {
+				// e.target.files가 유사배열이라서, forEach를 못쓴다. forEach는 인자가 콜백함수.
+				imageFormData.append('image', f); // { image: [file1, file2] }
+			});
+			this.$store.dispatch('posts/uploadImages', imageFormData);
+		},
+		onRemoveImage(idx) {
+			this.$store.commit('posts/removeImagePath', idx);
+		},
+	},
 };
 </script>
 
